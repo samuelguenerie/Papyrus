8bb833293069a1a02d0c5b323583cefd8f9ebb61
Samuel Guénerie <contact@samuelguenerie.com>
2023-03-09 21:25:45 +0100

    feat: delete example files from the project installation

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <header>
+      <h1>Papyrus</h1>
+    </header>
   </main>
 </template>
